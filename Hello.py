8e647a0f801ec1ca575079c8b22a40772e2e46cc
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2023-09-22 10:41:51 -0600

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -24,7 +24,7 @@ def run():
         page_icon="👋",
     )
 
-    st.write("# Alex Welcome to Streamlit! 👋")
+    st.write("# *** Alex Welcome to Streamlit! ***")
 
     st.sidebar.success("Select a demo above.")
 
